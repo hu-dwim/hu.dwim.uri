1186c5e4084290c14cb38a68cb80b601b7ab7a68
attila.lendvai <attila.lendvai@gmail.com>
2011-12-22 08:39:41 +0000

    test/ update for recent changes/fixes

--- test/parsing.lisp
+++ test/parsing.lisp
@@ -19,7 +19,7 @@
 
 (def test print-and-compare (expected-uri-string &key scheme host port path fragment)
   (is (string= expected-uri-string
-               (finishes (uri/print-to-string (make-uri :scheme scheme :host host :port port :path path :fragment fragment))))))
+               (finishes (print-uri-to-string (make-uri :scheme scheme :host host :port port :path path :fragment fragment))))))
 
 (def test test/uri/simple ()
   (parse-and-compare "foo/bar"
@@ -38,20 +38,16 @@
                      :scheme "http" :host "example.com" :port 42 :path '("foo" "bar") :fragment "asdf"))
 
 (def test test/uri/idna ()
-  (flet ((entry (escaped-uri-string printed-form &rest components)
-           (is (string= (uri/print-to-string (apply 'parse-and-compare escaped-uri-string components))
-                        printed-form))))
+  (flet ((entry (uri-string &rest components)
+           (is (string= (print-uri-to-string (apply 'parse-and-compare uri-string components))
+                        uri-string))))
     (entry "http://r%C3%A4ksm%C3%B6rg%C3%A5s.josefsson.org"
-           "http://xn--rksmrgs-5wao1o.josefsson.org"
            :scheme "http" :host "räksmörgås.josefsson.org")
     (entry "http://%E7%B4%8D%E8%B1%86.w3.mag.keio.ac.jp"
-           "http://xn--99zt52a.w3.mag.keio.ac.jp"
            :scheme "http" :host "納豆.w3.mag.keio.ac.jp")
     (entry "http://www.%E3%81%BB%E3%82%93%E3%81%A8%E3%81%86%E3%81%AB%E3%81%AA%E3%81%8C%E3%81%84%E3%82%8F%E3%81%91%E3%81%AE%E3%82%8F%E3%81%8B%E3%82%89%E3%81%AA%E3%81%84%E3%81%A9%E3%82%81%E3%81%84%E3%82%93%E3%82%81%E3%81%84%E3%81%AE%E3%82%89%E3%81%B9%E3%82%8B%E3%81%BE%E3%81%A0%E3%81%AA%E3%81%8C%E3%81%8F%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%81%9F%E3%82%8A%E3%81%AA%E3%81%84.w3.mag.keio.ac.jp/"
-           "http://www.xn--n8jaaaaai5bhf7as8fsfk3jnknefdde3fg11amb5gzdb4wi9bya3kc6lra.w3.mag.keio.ac.jp"
            :scheme "http" :host "www.ほんとうにながいわけのわからないどめいんめいのらべるまだながくしないとたりない.w3.mag.keio.ac.jp")
     (entry "http://%E3%81%BB%E3%82%93%E3%81%A8%E3%81%86%E3%81%AB%E3%81%AA%E3%81%8C%E3%81%84%E3%82%8F%E3%81%91%E3%81%AE%E3%82%8F%E3%81%8B%E3%82%89%E3%81%AA%E3%81%84%E3%81%A9%E3%82%81%E3%81%84%E3%82%93%E3%82%81%E3%81%84%E3%81%AE%E3%82%89%E3%81%B9%E3%82%8B%E3%81%BE%E3%81%A0%E3%81%AA%E3%81%8C%E3%81%8F%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%81%9F%E3%82%8A%E3%81%AA%E3%81%84.%E3%81%BB%E3%82%93%E3%81%A8%E3%81%86%E3%81%AB%E3%81%AA%E3%81%8C%E3%81%84%E3%82%8F%E3%81%91%E3%81%AE%E3%82%8F%E3%81%8B%E3%82%89%E3%81%AA%E3%81%84%E3%81%A9%E3%82%81%E3%81%84%E3%82%93%E3%82%81%E3%81%84%E3%81%AE%E3%82%89%E3%81%B9%E3%82%8B%E3%81%BE%E3%81%A0%E3%81%AA%E3%81%8C%E3%81%8F%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%81%9F%E3%82%8A%E3%81%AA%E3%81%84.%E3%81%BB%E3%82%93%E3%81%A8%E3%81%86%E3%81%AB%E3%81%AA%E3%81%8C%E3%81%84%E3%82%8F%E3%81%91%E3%81%AE%E3%82%8F%E3%81%8B%E3%82%89%E3%81%AA%E3%81%84%E3%81%A9%E3%82%81%E3%81%84%E3%82%93%E3%82%81%E3%81%84%E3%81%AE%E3%82%89%E3%81%B9%E3%82%8B%E3%81%BE%E3%81%A0%E3%81%AA%E3%81%8C%E3%81%8F%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%81%9F%E3%82%8A%E3%81%AA%E3%81%84.w3.mag.keio.ac.jp/"
-           "http://xn--n8jaaaaai5bhf7as8fsfk3jnknefdde3fg11amb5gzdb4wi9bya3kc6lra.xn--n8jaaaaai5bhf7as8fsfk3jnknefdde3fg11amb5gzdb4wi9bya3kc6lra.xn--n8jaaaaai5bhf7as8fsfk3jnknefdde3fg11amb5gzdb4wi9bya3kc6lra.w3.mag.keio.ac.jp"
            :scheme "http" :host "ほんとうにながいわけのわからないどめいんめいのらべるまだながくしないとたりない.ほんとうにながいわけのわからないどめいんめいのらべるまだながくしないとたりない.ほんとうにながいわけのわからないどめいんめいのらべるまだながくしないとたりない.w3.mag.keio.ac.jp")))
 
 (def test test/uri/escaping ()
@@ -64,4 +60,6 @@
   (signals error (parse-uri uri-string)))
 
 (def test test/uri/illegal ()
-  (illegal-uri "htt!p://example.com/"))
+  (illegal-uri "htt!p://example.com/")
+  ;; unicode stuff and other illegal chars must be percent-encoded
+  (illegal-uri "http://example.com/áéaдвлбы"))
